ea7af3bec9f23e07b99b5aef12c95232a6fe2472
Ubuntu <klogserver@klogserver.dshywofymvxepiseljrgc1xwte.xx.internal.cloudapp.net>
2019-11-07 03:22:49 +0000

    update server config

--- api_demo/cloudmedia_create.py
+++ api_demo/cloudmedia_create.py
@@ -15,15 +15,15 @@ host = host_url_maker(sys.argv)
 api_url = "{}/api/0/config/cloudmedia".format(host)
 method = HTTPMethod.POST
 
-media_names = ["student1_image1.gif", "student1_image2.gif", "student1_image3.gif", "student1_image4.gif", "student1_image5.gif"]
-#media_names = ["student2_video1.mp4", "student2_video2.mp4", "student2_video3.mp4", "student2_video4.mp4"]
+#media_names = ["student1_image1.gif", "student1_image2.gif", "student1_image3.gif", "student1_image4.gif", "student1_image5.gif"]
+media_names = ["student2_video1.mp4", "student2_video2.mp4", "student2_video3.mp4", "student2_video4.mp4"]
 for media_name in media_names:
     params = {
-        "media_type": "image",
+        "media_type": "video",
         "media_name": media_name,
         "media_url": "https://klogresourcediag159.blob.core.windows.net/klog-cloud-media/{}".format(media_name),
         "rank_score": random.uniform(50, 100),
-        "student_pid": "102030405060708090000001"
+        "student_pid": "102030405060708090000002"
     }
 
     print("[create cloudmedia for {}]".format(media_name))
